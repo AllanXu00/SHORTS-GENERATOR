d06be4a1683fb31154bc6c856baae9a3bb798d77
AllanXu00 <AllanXu00@users.noreply.github.com>
2017-01-11 15:49:04 -0500

    Create Basic-Conversions.py

---
+++ Basic-Conversions.py
@@ -0,0 +1,12 @@
+#Given a number and a base convert it into base 2, 8, 10, 16
+import random as r
+print "Convert the following numbers into bases 2, 8, 10, and 16: "
+for j in range (20): 
+	lim = r.randint(2, 10)
+	leng = r.randint (2, 10)
+	b = ""
+	for i in range (leng):
+		a = r.randint(0, lim-1)
+		c = str(a)
+		b += c
+	print b, "in base", lim
